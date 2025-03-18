23110463ba7fdffdd8cb479b9aaef08e4f2222b8
Wai Wong <daomen@gmail.com>
2025-03-18 20:57:46 +0100

    Code cleaning

--- retrieve.py
+++ retrieve.py
@@ -54,55 +54,66 @@ def get_coordinates(city, country):
     
 
 def get_weather_data(lat, long):
-  # Setup the Open-Meteo API client with cache and retry on error
-  cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-  retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-  openmeteo = openmeteo_requests.Client(session = retry_session)
+  file = "weather_data.csv"
+  if not Path(file).is_file():
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
 
-  # Make sure all required weather variables are listed here
-  # The order of variables in hourly or daily is important to assign them correctly below
-  url = "https://archive-api.open-meteo.com/v1/archive"
-  params = {
-    "latitude": lat,
-    "longitude": long,
-    "start_date": "2024-01-01",
-    "end_date": "2024-12-31",
-    "daily": ["temperature_2m_max", "temperature_2m_min", "weather_code", "precipitation_sum", "temperature_2m_mean"]
-  }
-  responses = openmeteo.weather_api(url, params=params)
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+      "latitude": lat,
+      "longitude": long,
+      "start_date": "2024-01-01",
+      "end_date": "2024-12-31",
+      "daily": ["temperature_2m_max", "temperature_2m_min", "weather_code", "precipitation_sum", "temperature_2m_mean"]
+    }
+    responses = openmeteo.weather_api(url, params=params)
 
-  # Process first location. Add a for-loop for multiple locations or weather models
-  response = responses[0]
-  print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-  print(f"Elevation {response.Elevation()} m asl")
-  print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
-  print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
-                # Process daily data. The order of variables needs to be the same as requested.
-  daily = response.Daily()
-  daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-  daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-  daily_weather_code = daily.Variables(2).ValuesAsNumpy()
-  daily_precipitation_sum = daily.Variables(3).ValuesAsNumpy()
-  daily_temperature_2m_mean = daily.Variables(4).ValuesAsNumpy()
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+    daily_weather_code = daily.Variables(2).ValuesAsNumpy()
+    daily_precipitation_sum = daily.Variables(3).ValuesAsNumpy()
+    daily_temperature_2m_mean = daily.Variables(4).ValuesAsNumpy()
 
-  daily_data = {"date": pd.date_range(
-    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-    end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-    freq = pd.Timedelta(seconds = daily.Interval()),
-    inclusive = "left"
-  )}
+    daily_data = {"date": pd.date_range(
+      start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+      end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+      freq = pd.Timedelta(seconds = daily.Interval()),
+      inclusive = "left"
+    )}
 
-  daily_data["temperature_2m_max"] = daily_temperature_2m_max
-  daily_data["temperature_2m_min"] = daily_temperature_2m_min
-  daily_data["weather_code"] = daily_weather_code
-  daily_data["precipitation_sum"] = daily_precipitation_sum
-  daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+    daily_data["weather_code"] = daily_weather_code
+    daily_data["precipitation_sum"] = daily_precipitation_sum
+    daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
 
-  daily_dataframe = pd.DataFrame(data = daily_data)
+    df = pd.DataFrame(data = daily_data)
 
-  # Plot line chart including average, minimum and maximum temperature
-  daily_dataframe.plot(y=['temperature_2m_min', 'temperature_2m_max'])
+    # Save to CSV file
+    csv_file = "weather_data.csv"
+    df.to_csv(csv_file, index=True)  # Index=True to keep the date column
+  else:
+    # Read from a csv file
+    df = pd.read_csv(file)
+
+  # Plot line chart including minimum and maximum temperature
+  df.plot(y=['temperature_2m_min', 'temperature_2m_max'])
+  # df["date"] = pd.to_datetime(df["date"]) 
+  
   plt.show()
 
 get_coordinates(city, country)
